ed2362c49854c05f1f35310804427ea544eed349
anwesha <anwes@LAPTOP-2TU5J9CV>
2023-11-15 11:28:19 +0530

    Dummy project practice

---
+++ src/main/java/sample/Prints.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class Prints {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hii");
+		System.out.println("From Git Hub Account");
+
+	}
+
+}
